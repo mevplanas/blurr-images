51989b788b6594448b1988c2aa4dedbf8e34f97b
Austėja Burmonaitė <austeja.burmonaite@idvilnius.lt>
2023-12-05 09:45:45 +0200

    updated download script

--- download_data_azure.py
+++ download_data_azure.py
@@ -13,20 +13,11 @@ if __name__ == '__main__':
     with open(os.path.join(current_path, 'configuration.yml'), 'r') as stream:
         conf = yaml.safe_load(stream)
 
-    container_path = conf['AZURE_STORAGE_CREDENTIALS']['CONTAINERNAME']
-    blob_path = conf['AZURE_STORAGE_CREDENTIALS']['BLOBNAME']
-    account_url = conf['AZURE_STORAGE_CREDENTIALS']['ACCOUNTURL']
-    storage_account_key = conf['AZURE_STORAGE_CREDENTIALS']['STORAGEACCOUNTKEY']
-    path_for_download = conf['PATH_FOR_AZURE_FOLDER']
-
     # Reading the connection string 
     conn_string = conf['AZURE_STORAGE_CREDENTIALS']['CONSTRING']
 
     # Making a connection to azure blob 
     conn_blob = BlobServiceClient.from_connection_string(conn_string)
-
-    # Infering the directory from which to download images
-    dir_azure = conf['PATH_TO_AZURE_DATA']
 
     # Listing all the directories in connection blob
     azure_dirs = open("azure_dirs","w", encoding="utf-8")
