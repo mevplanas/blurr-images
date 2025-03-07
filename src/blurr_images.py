a85e44b25e457802356d001aa98b7e4af5c270e9
Eligijus <elasorama@gmail.com>
2025-03-07 10:05:15 +0200

    feat: added a new and shiny YOLO implementation

--- src/blurr_images.py
+++ src/blurr_images.py
@@ -1,201 +1,199 @@
-# OS traversal 
-import os 
+# OS traversal
+import os
 
-# Importing the NAS vision model 
-import super_gradients.training.models
+# Importing the computer vision model
+from ultralytics import YOLO
 
-# IMG reading 
+# IMG reading
 import cv2
 
 # Iteration tracking
 from tqdm import tqdm
 
-# YAML reading 
+# YAML reading
 import yaml
+
+# Image data parsing
+from exif import Image
 
 # Blob storage
 from azure.storage.blob import BlobServiceClient
+
+# Hardcoding Vilnius altitude
+VILNIUS_ALTITUDE = 112
 
 # Reading the configuration file
 with open("configuration.yml", "r") as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
-# Getting the sg model 
-yolo_nas = super_gradients.training.models.get(config['NAS_MODEL'], pretrained_weights="coco")
+# Saving the class names
+idx2class = config["CLASS_NAMES"]
+class2idx = {v: k for k, v in idx2class.items()}
+
+# Getting the sg model
+yolo_nas = YOLO(config["YOLO_MODEL"])
 
 # Defining the classes to blurr
-classes_to_blur = config['CLASSES_TO_BLURR']
+classes_to_blur = config["CLASSES_TO_BLURR"]
 
-# Extracting the blurr intensity 
-blurr_intensity = int(config['BLURR_INTENSITY'])
+# Extracting the blurr intensity
+blurr_intensity = int(config["BLURR_INTENSITY"])
 
-# Extracting the input connection string and container name 
-input_connection_string = config['INPUT_CONNECTION_STRING']
-input_container_name = config['INPUT_CONTAINER_NAME']
+# Extracting the input connection string and container name
+input_connection_string = config["INPUT_CONNECTION_STRING"]
+input_container_name = config["INPUT_CONTAINER_NAME"]
 
 # Extracting the output connection string and container name
-output_connection_string = config['OUTPUT_CONNECTION_STRING']
-output_container_name = config['OUTPUT_CONTAINER_NAME']
+output_connection_string = config["OUTPUT_CONNECTION_STRING"]
+output_container_name = config["OUTPUT_CONTAINER_NAME"]
+
 
 def is_blob_image(blob_name: str) -> bool:
     """
     Checks if the blob is an image
     """
-    return blob_name.endswith('.jpg') or blob_name.endswith('.png') or blob_name.endswith('.jpeg') or blob_name.endswith('.JPG') or blob_name.endswith('.PNG') or blob_name.endswith('.JPEG')
+    return (
+        blob_name.endswith(".jpg")
+        or blob_name.endswith(".png")
+        or blob_name.endswith(".jpeg")
+        or blob_name.endswith(".JPG")
+        or blob_name.endswith(".PNG")
+        or blob_name.endswith(".JPEG")
+    )
 
-# Defining the pipeline 
+
+# Defining the pipeline
 def pipeline() -> None:
     # Infering the current directory
     current_dir = os.path.dirname(os.path.realpath(__file__))
 
-    # Initial class names 
-    class_names = None 
+    # Creating a log.txt file to log the processed blobs
+    log_file = os.path.join(current_dir, "..", "log.txt")
+    log = []
+    if not os.path.exists(log_file):
+        with open(log_file, "w") as f:
+            f.write("")
+    else:
+        log = open(log_file, "r").read().split("\n")
+
+    # Initial class names
+    class_names = None
     class_names_dict = {}
 
-    # Creating the connection 
-    blob_service_client = BlobServiceClient.from_connection_string(input_connection_string)
+    # Creating the connection
+    blob_service_client = BlobServiceClient.from_connection_string(
+        input_connection_string
+    )
 
     # Getting the container
     container_client = blob_service_client.get_container_client(input_container_name)
 
-    # Listing all the blobs 
+    # Listing all the blobs
     blobs = container_client.list_blobs()
 
     # Only leaving the images
     blobs = [blob for blob in blobs if is_blob_image(blob.name)]
 
-    # Creating the input directory 
-    input_dir = os.path.join(current_dir, '..', 'input', "images")
+    # Dropping the blobs that are in the 00_UNSORTED directory
+    blobs = [blob for blob in blobs if "00_UNSORTED" not in blob.name]
+
+    # Creating the unique blob names and only processing the ones that are not in the log
+    blobs = [blob for blob in blobs if blob.name not in log]
+
+    # Creating the input directory
+    input_dir = os.path.join(current_dir, "..", "input")
     os.makedirs(input_dir, exist_ok=True)
 
-    # Creating the output dir 
-    postprocessed_images_dir = os.path.join(current_dir, '..', 'output', "images")
-    os.makedirs(postprocessed_images_dir, exist_ok=True)
+    # The logic is this:
+    # For each image:
+    # 1. Download the image
+    # 2. Predict the bounding boxes
+    # 3. If the bounding box is in the classes to blurr, blurr the image
+    # 4. Save the image back to the original directory in azure blob
 
-    # Downloading the images
-    for blob in tqdm(blobs, desc='Downloading the images'): 
+    for blob in tqdm(blobs):
         # Getting the base name
         base_name = os.path.basename(blob.name)
 
-        # Getting everything except the base name from blob name 
-        blob_dir = blob.name.replace(base_name, '')
+        # Getting everything except the base name from blob name
+        blob_dir = blob.name.replace(base_name, "")
 
         # Creating the directory
-        os.makedirs(os.path.join(input_dir, blob_dir), exist_ok=True)
+        os.makedirs(os.path.join(current_dir, "..", "input", blob_dir), exist_ok=True)
 
-        # Defining the path to the image 
-        path = os.path.join(input_dir, blob_dir, base_name)
+        # Defining the path to the image
+        path = os.path.join(current_dir, "..", "input", blob_dir, base_name)
 
-        # Downloading the blob 
-        blob_client = blob_service_client.get_blob_client(container=input_container_name, blob=blob.name)
+        # Downloading the blob
+        blob_client = blob_service_client.get_blob_client(
+            container=input_container_name, blob=blob.name
+        )
         with open(path, "wb") as f:
             f.write(blob_client.download_blob().readall())
 
-    # Listing all the images in the input dir up to the most granular level
-    # Getting all the directories
-    # Iterating over the directories
-    # Getting all the images in the directory
-    # Adding them to the list of images
-    images = []
-    for root, _, files in os.walk(input_dir):
-        if len(files) > 0:
-            for file in files:
-                if is_blob_image(file):
-                    # Getting the path to the image
-                    path = os.path.join(root, file)
+        # Reading the exif image data
+        img_metadata = Image(path)
 
-                    # Getting the relative path
-                    rel_path = os.path.relpath(path, input_dir)
+        # Getting the altitude
+        altitude = img_metadata.gps_altitude
 
-                    # Adding the relative path to the list of images
-                    images.append(rel_path)   
+        # Calculating the real height from ground
+        height_from_ground = altitude - VILNIUS_ALTITUDE
 
-    # Iterating over the images 
-    for img in tqdm(images, desc='Blurring the images'): 
+        # Only blurring, if the image passes the treshold
+        if height_from_ground < config["MINIMUM_HEIGHT_FROM_GROUND"]:
 
-        # Reading the image with cv2
-        img_cv = cv2.imread(os.path.join(input_dir, img))
+            # Predicting on the downloaded image
+            hat = yolo_nas.predict(
+                path,
+                conf=config.get("MODEL_CONFIDENCE", 0.15),
+                iou=config.get("MODEL_IOU", 0.9),
+            )
 
-        # Predicting 
-        hat = yolo_nas.predict(os.path.join(input_dir, img))
+            # Getting the bounding boxes
+            boxes = hat[0].boxes
 
-        if class_names is None:
+            # Getting the labels
+            labels = hat[0].boxes.cls
+            labels = labels.cpu().numpy()
+            label_names = [idx2class[label] for label in labels]
 
-            # Saving the list of class names
-            class_names = hat[0].class_names 
+            # Extracting the xyxy coordinates
+            bboxes = boxes.xyxy
 
-            # Making a dictionary where the key is the index and the value is the class name
-            class_names_dict = {i: class_names[i] for i in range(len(class_names))}
-        
-        # Getting the bounding boxes
-        # Extracting the predictions
-        predictions = hat[0].prediction
+            # Bluring
+            img_cv = cv2.imread(path)
 
-        # Saving the bboxes 
-        bboxes = predictions.bboxes_xyxy
-
-        # Saving the labels 
-        labels = predictions.labels
-
-        # Getting the bounding boxes
-        if len(bboxes) > 0:
-            # Iterating over the boxes
             for i, box in enumerate(bboxes):
-                try:
-                    # Getting the class name
-                    class_name = class_names_dict[labels[i]]
+                if label_names[i] in classes_to_blur:
+                    # Getting the x, y, w, h
+                    x0, y0, x1, y1 = box[0], box[1], box[2], box[3]
 
-                    if class_name in classes_to_blur:
+                    # Converting to int
+                    x0, y0, x1, y1 = int(x0), int(y0), int(x1), int(y1)
 
-                        # Getting the x, y, w, h
-                        x0, y0, x1, y1 = box[0], box[1], box[2], box[3]
+                    # Blurring the image
+                    img_cv[y0:y1, x0:x1] = cv2.blur(
+                        img_cv[y0:y1, x0:x1], (blurr_intensity, blurr_intensity)
+                    )
 
-                        # Converting to int
-                        x0, y0, x1, y1 = int(x0), int(y0), int(x1), int(y1)
+            # Overwritting the original image
+            cv2.imwrite(path, img_cv)
 
-                        # Blurring the image
-                        img_cv[y0:y1, x0:x1] = cv2.blur(img_cv[y0:y1, x0:x1], (blurr_intensity, blurr_intensity))
+            # Adding all the exif information to the image
+            img_metadata.save(path)
 
-                except Exception as e:
-                    print(e)
-                    continue
+            # Uploading to the same place in the blob
+            with open(path, "rb") as data:
+                container_client.upload_blob(name=blob.name, data=data)
 
-        # Getting the base name of the image 
-        base_name = os.path.basename(img)
+        # Deleting the image from local storage
+        os.remove(path)
 
-        # Getting everything except the base name from blob name
-        blob_dir = img.replace(base_name, '')
+        # Appending the blob name to the log.txt file
+        with open(log_file, "a") as f:
+            f.write(blob.name + "\n")
 
-        # Creating the output directory 
-        output_dir = os.path.join(postprocessed_images_dir, blob_dir)
-        os.makedirs(output_dir, exist_ok=True)
 
-        # Defining the full path to the image
-        image_path = os.path.join(output_dir, base_name)
-
-        # Saving the image
-        cv2.imwrite(image_path, img_cv)
-
-    # Uploading the blurred images to the output container
-    # Creating the connection
-    blob_service_client = BlobServiceClient.from_connection_string(output_connection_string)
-
-    # Getting the container
-    container_client = blob_service_client.get_container_client(output_container_name)
-
-    # Iterating over the output images and uploading 
-    for root, _, files in os.walk(postprocessed_images_dir):
-        if len(files) > 0:
-            for file in tqdm(files, desc=f'Uploading the images from {postprocessed_images_dir} to {output_container_name}'):
-                # Defining the path to the image
-                path = os.path.join(root, file)
-
-                # Getting the relative path
-                rel_path = os.path.relpath(path, postprocessed_images_dir)
-
-                # Uploading the blob
-                with open(path, "rb") as data:
-                    container_client.upload_blob(name=rel_path, data=data)
-
-if __name__ == '__main__': 
+if __name__ == "__main__":
     pipeline()
--- src/blurr_videos.py
+++ src/blurr_videos.py
@@ -1,107 +1,124 @@
-# OS traversal 
-import os 
+# OS traversal
+import os
 import shutil
 
-# Importing the NAS vision model 
+# Importing the NAS vision model
 import super_gradients.training.models
 
-# IMG reading 
+# IMG reading
 import cv2
 
 # Iteration tracking
 from tqdm import tqdm
 
-# YAML reading 
+# YAML reading
 import yaml
 
-# TMP directory creation 
+# TMP directory creation
 import tempfile
 
-# Blob services 
+# Blob services
 from azure.storage.blob import BlobServiceClient
 
 # Reading the configuration file
 with open("configuration.yml", "r") as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 
-# Getting the sg model 
-yolo_nas = super_gradients.training.models.get(config['NAS_MODEL'], pretrained_weights="coco")
+# Getting the sg model
+yolo_nas = super_gradients.training.models.get(
+    config["NAS_MODEL"], pretrained_weights="coco"
+)
 
 # Defining the classes to blurr
-classes_to_blur = config['CLASSES_TO_BLURR']
+classes_to_blur = config["CLASSES_TO_BLURR"]
 
-# Extracting the blurr intensity 
-blurr_intensity = config['BLURR_INTENSITY']
+# Extracting the blurr intensity
+blurr_intensity = config["BLURR_INTENSITY"]
 
-# Extracting the input connection string and container name 
-input_connection_string = config['INPUT_CONNECTION_STRING']
-input_container_name = config['INPUT_CONTAINER_NAME']
+# Extracting the input connection string and container name
+input_connection_string = config["INPUT_CONNECTION_STRING"]
+input_container_name = config["INPUT_CONTAINER_NAME"]
 
 # Extracting the output connection string and container name
-output_connection_string = config['OUTPUT_CONNECTION_STRING']
-output_container_name = config['OUTPUT_CONTAINER_NAME']
+output_connection_string = config["OUTPUT_CONNECTION_STRING"]
+output_container_name = config["OUTPUT_CONTAINER_NAME"]
+
 
 def is_blob_video(blob_name: str) -> bool:
     """
     Checks if the blob is an image
     """
-    return blob_name.endswith('.MP4') or blob_name.endswith('.mp4') or blob_name.endswith('.AVI') or blob_name.endswith('.avi') or blob_name.endswith('.MOV') or blob_name.endswith('.mov')
+    return (
+        blob_name.endswith(".MP4")
+        or blob_name.endswith(".mp4")
+        or blob_name.endswith(".AVI")
+        or blob_name.endswith(".avi")
+        or blob_name.endswith(".MOV")
+        or blob_name.endswith(".mov")
+    )
 
-def pipeline(dl_video: bool = True) -> None: 
+
+def pipeline(dl_video: bool = True) -> None:
     # Getting the current directory
     current_dir = os.path.dirname(os.path.realpath(__file__))
 
-    # Initial class names 
-    class_names = None 
+    # Initial class names
+    class_names = None
     class_names_dict = {}
 
-    # Creating the connection 
-    blob_service_client = BlobServiceClient.from_connection_string(input_connection_string)
+    # Creating the connection
+    blob_service_client = BlobServiceClient.from_connection_string(
+        input_connection_string
+    )
 
     # Getting the container
     container_client = blob_service_client.get_container_client(input_container_name)
 
-    # Listing all the blobs 
+    # Listing all the blobs
     blobs = container_client.list_blobs()
 
     # Only leaving the videos
     blobs = [blob for blob in blobs if is_blob_video(blob.name)]
 
-    # Creating the input directory 
-    input_dir = os.path.join(current_dir, '..', 'input', "video")
+    # Creating the input directory
+    input_dir = os.path.join(current_dir, "..", "input", "video")
     os.makedirs(input_dir, exist_ok=True)
 
-    # Creating the output dir 
-    postprocessed_videos_dir = os.path.join(current_dir, '..', 'output', "video")
+    # Creating the output dir
+    postprocessed_videos_dir = os.path.join(current_dir, "..", "output", "video")
     os.makedirs(postprocessed_videos_dir, exist_ok=True)
 
     # Downloading the videos
     if dl_video:
-        for blob in tqdm(blobs, desc='Downloading the videos'): 
+        for blob in tqdm(blobs, desc="Downloading the videos"):
             # Getting the base name
             base_name = os.path.basename(blob.name)
 
-            # Getting everything except the base name from blob name 
-            blob_dir = blob.name.replace(base_name, '')
+            # Getting everything except the base name from blob name
+            blob_dir = blob.name.replace(base_name, "")
 
             # Creating the directory
             os.makedirs(os.path.join(input_dir, blob_dir), exist_ok=True)
 
-            # Defining the path to the image 
+            # Defining the path to the image
             path = os.path.join(input_dir, blob_dir, base_name)
 
-            # Downloading the blob 
-            blob_client = blob_service_client.get_blob_client(container=input_container_name, blob=blob.name)
+            # Downloading the blob
+            blob_client = blob_service_client.get_blob_client(
+                container=input_container_name, blob=blob.name
+            )
             with open(path, "wb") as f:
                 f.write(blob_client.download_blob().readall())
 
-    # Creating the containers for output 
-    blob_service_client = BlobServiceClient.from_connection_string(output_connection_string)
+    # Creating the containers for output
+    blob_service_client = BlobServiceClient.from_connection_string(
+        output_connection_string
+    )
 
     # Getting the container
     container_client = blob_service_client.get_container_client(output_container_name)
 
-    # Placeholder for class names 
+    # Placeholder for class names
     class_names = None
 
     # Listing all the videps in the input dir up to the most granular level
@@ -121,7 +138,7 @@ def pipeline(dl_video: bool = True) -> None:
                     rel_path = os.path.relpath(path, input_dir)
 
                     # Adding the relative path to the list of images
-                    videos.append(rel_path)   
+                    videos.append(rel_path)
 
     # Iterating over the videos
     for video in videos:
@@ -131,51 +148,51 @@ def pipeline(dl_video: bool = True) -> None:
         # Creating a tmp dir for frames
         tmp_dir_frames = tempfile.mkdtemp()
 
-        # Reading the video 
+        # Reading the video
         cap = cv2.VideoCapture(path)
 
-        # Spliting the video by frame 
+        # Spliting the video by frame
         i = 0
-        while(cap.isOpened()):
+        while cap.isOpened():
             ret, frame = cap.read()
             if ret == False:
                 break
-            cv2.imwrite(os.path.join(tmp_dir_frames, f'{str(i)}.jpg'), frame)
-            i+=1
+            cv2.imwrite(os.path.join(tmp_dir_frames, f"{str(i)}.jpg"), frame)
+            i += 1
 
         # Iterating over the frames and blurring
-        # Iterating over the images 
+        # Iterating over the images
         frames = os.listdir(tmp_dir_frames)
 
         # Creating tmp dir for postprocessed images
         tmp_dir_postprocessed_images = tempfile.mkdtemp()
 
-        for img in tqdm(frames, desc=f'Blurring the images for video: {video}'): 
+        for img in tqdm(frames, desc=f"Blurring the images for video: {video}"):
             # Defining the path to image
             path = os.path.join(tmp_dir_frames, img)
 
             # Reading the image with cv2
             img_cv = cv2.imread(path)
 
-            # Predicting 
+            # Predicting
             hat = yolo_nas.predict(path)
 
             if class_names is None:
 
                 # Saving the list of class names
-                class_names = hat[0].class_names 
+                class_names = hat[0].class_names
 
                 # Making a dictionary where the key is the index and the value is the class name
                 class_names_dict = {i: class_names[i] for i in range(len(class_names))}
-            
+
             # Getting the bounding boxes
             # Extracting the predictions
             predictions = hat[0].prediction
 
-            # Saving the bboxes 
+            # Saving the bboxes
             bboxes = predictions.bboxes_xyxy
 
-            # Saving the labels 
+            # Saving the labels
             labels = predictions.labels
 
             # Getting the bounding boxes
@@ -195,7 +212,9 @@ def pipeline(dl_video: bool = True) -> None:
                             x0, y0, x1, y1 = int(x0), int(y0), int(x1), int(y1)
 
                             # Blurring the image
-                            img_cv[y0:y1, x0:x1] = cv2.blur(img_cv[y0:y1, x0:x1], (blurr_intensity, blurr_intensity))
+                            img_cv[y0:y1, x0:x1] = cv2.blur(
+                                img_cv[y0:y1, x0:x1], (blurr_intensity, blurr_intensity)
+                            )
 
                     except Exception as e:
                         print(e)
@@ -215,18 +234,18 @@ def pipeline(dl_video: bool = True) -> None:
         base_name = os.path.basename(video)
 
         # Getting the directory of the video
-        video_dir = video.replace(base_name, '')
+        video_dir = video.replace(base_name, "")
 
-        # Creatign the dir 
+        # Creatign the dir
         os.makedirs(os.path.join(postprocessed_videos_dir, video_dir), exist_ok=True)
 
         # Listing the images
         images = os.listdir(tmp_dir_postprocessed_images)
 
-        # Creating the image dictionary where the key is the image name and the image index 
+        # Creating the image dictionary where the key is the image name and the image index
         image_dict = dict()
         for image in images:
-            image_dict[image] = int(image.split('.')[0])
+            image_dict[image] = int(image.split(".")[0])
 
         # Sorting the images by the index
         images = sorted(images, key=lambda x: image_dict[x])
@@ -235,12 +254,17 @@ def pipeline(dl_video: bool = True) -> None:
         fps = 24
 
         # Defining the size of the video
-        # Reading the first image to get the sizes 
+        # Reading the first image to get the sizes
         img = cv2.imread(os.path.join(tmp_dir_postprocessed_images, images[0]))
         size = (img.shape[1], img.shape[0])
 
         # Defining the video writer
-        out = cv2.VideoWriter(os.path.join(postprocessed_videos_dir, video_dir, base_name), cv2.VideoWriter_fourcc(*'DIVX'), fps, size)
+        out = cv2.VideoWriter(
+            os.path.join(postprocessed_videos_dir, video_dir, base_name),
+            cv2.VideoWriter_fourcc(*"DIVX"),
+            fps,
+            size,
+        )
 
         # Iterating over the images
         for image in tqdm(images):
@@ -260,7 +284,11 @@ def pipeline(dl_video: bool = True) -> None:
         shutil.rmtree(tmp_dir_postprocessed_images)
 
         # Uploading the video
-        with open(os.path.join(postprocessed_videos_dir, video_dir, base_name), "rb") as data:
+        with open(
+            os.path.join(postprocessed_videos_dir, video_dir, base_name), "rb"
+        ) as data:
             container_client.upload_blob(name=video, data=data)
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     pipeline()
